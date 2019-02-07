26954de4f5c7754fb35a01234cf4657b499b698f
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-02-07 19:44:12 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Arafat</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="icon" type="text/css" href="favicon.ico">
+</head>
+<body>
+
+<div class="top-container">
+    <img class="top-cloud" src="images/cloude.jpg" alt="cloud-img">
+    <h1 class="arafat">I'm Arafat</h1>
+    <p class="programmer">a programmer.</p>
+    <img class="bottom-cloud" src="images/cloude.jpg" alt="cloud-img">
+    <img class="mountain" src="images/mountain.jpg" alt="mountain-img">
+</div>
+
+
+</body>
+</html>
